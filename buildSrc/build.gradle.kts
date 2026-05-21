a0912d6b28f3b80aab04ad5f1efb81eed58a8887
Rémi Bleuse <remi.bleuse@gmail.com>
2026-05-21 10:45:59 +0900

    use kotlin version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,8 +8,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.21")
-    implementation("org.jetbrains.kotlin:kotlin-allopen:2.3.21")
+    implementation(libs.kotlin.gradle.plugin)
+    implementation(libs.kotlin.allopen)
     // Make the type-safe `libs` accessor visible inside precompiled script plugins.
     implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
 }
